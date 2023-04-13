a22f3b3f788b51a76760ed81e29ea914f52c905f
Ankush Jain Lunawath <ankushjain369@gmail.com>
2023-04-13 20:27:19 +0530

    Clock Project

---
+++ clock.js
@@ -0,0 +1,21 @@
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+
+
+setInterval(() => {
+    date = new Date();
+
+    htime = date.getHours();
+    mtime = date.getMinutes();
+    stime = date.getSeconds();
+
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
